3c3cafc3fb3ec4404049dcfd39782c5c83972fbb
iamfortune <ikechifortune@gmail.com>
2020-05-15 08:50:10 +0100

    inital commit

---
+++ Number_Guesser/app.js
@@ -0,0 +1,101 @@
+/*
+GAME FUNCTION
+- Player must guess a number between the min and the max
+- player gets a certain amount of guesses 
+- Notify player of guesses remaining 
+- Notify the player of the correct answer if loose
+- Let player chose to play again 
+*/
+
+// Game values 
+let min = 1,
+    max = 10,
+    winningNum = getWinningNum,
+    guessesLeft = 3;
+
+
+    // UI elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num')  ,
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message =  document.querySelector('.message');
+
+// Assign UI min and max 
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Play Again event listener 
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
+
+// Listen for Guess 
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value);
+    // Validate 
+    if(isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    // Check if won 
+    if(guess === winningNum){
+       // Game over - the player won 
+       gameOver(true, `${winningNum} is correct, YOU WIN!`)
+    } else {
+        // Wrong Number 
+        guessesLeft -= 1;
+
+        if(guessesLeft === 0){
+            // Game Over - Lost 
+
+            gameOver(false, `Game over you lost. The correct number was ${winningNum}`, 'red');
+        } else {
+            // Game continues - answer wrong
+
+            // Change border color 
+            guessInput.style.borderColor = 'red';
+
+            // Clear Input 
+            guessInput.value = '';
+
+            // Tell user its the wrong number 
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+        }
+    }
+});
+
+
+
+function gameOver(won, msg){
+    let color; 
+    won === true ? color = 'green' : color === 'red';
+
+    // Disable the input 
+    guessInput.disabled = true;
+    // Change border color 
+    guessInput.style.borderColor = 'color';
+    // Set text color 
+    message.style.color = color;
+    // Let the user know that they won 
+    setMessage(msg);
+
+    // Play Again
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
+
+// Get Winning Num 
+function getWinningNum(){
+    
+}
+
+
+// Set message 
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
---
+++ Task List/app.js
@@ -0,0 +1,153 @@
+// Define Ui Variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Add and Load all event listeners (instead of putting it in the global scope)
+loadEventListeners();
+
+function loadEventListeners() {
+  // DOM load event
+  document.addEventListener("DOMContentLoaded", getTasks);
+
+  // add task event
+  form.addEventListener("submit", addTask);
+  // add listener for remove task item
+  taskList.addEventListener("click", removeTask);
+  // add listener for clear all tasks
+  clearBtn.addEventListener("click", clearTasks);
+  // filter tasks event
+  filter.addEventListener("keyup", filterTasks);
+}
+
+// get tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task) {
+    // console.log(task)
+    // create li element
+    const li = document.createElement("li");
+    // add class
+    li.className = "collection-item";
+    // create next node and append to the li
+    li.appendChild(document.createTextNode(task));
+    // create new link element
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to the li
+    li.appendChild(link);
+
+    // Append the li to the ul
+    taskList.appendChild(li);
+  });
+}
+
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+  // create li element
+  const li = document.createElement("li");
+  // add class
+  li.className = "collection-item";
+  // create next node and append to the li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // create new link element
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  // add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to the li
+  li.appendChild(link);
+
+  // Append the li to the ul
+  taskList.appendChild(li);
+
+  // store in local storage
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
+
+// store task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.className === "delete-item secondary-content") {
+    e.target.parentElement.parentElement.remove();
+
+    // remove from local storage
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+  }
+}
+
+// remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  // console.log(taskItem);
+
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function clearTasks(e) {
+  // one way
+  // taskList.innerHTML = '';
+  // another way- actually faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  clearTasksFromLocalStorage();
+}
+
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  // query selector all returns nodelist so can use foreach
+  // but if was html collection would need to convert it to array first
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    console.log(task.firstChild.textContent);
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+}
