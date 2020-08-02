93dfcb8c01f7b2f28deb5e134cfa20093e21898e
iamfortune <ikechifortune@gmail.com>
2020-08-02 19:46:31 +0100

    added event listeners to get form value

--- Book List/app.js
+++ Book List/app.js
@@ -1,4 +1,24 @@
 // Book constructor
-
+function Book(title, author, isbn) {
+    this.titl = title;
+    this.author = author;
+    this.isbn = isbn;
+}
 
 // UI constructor 
+function UI() {}
+
+
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit',
+    function (e) {
+        // Get form values 
+        const title = document.getElementById('title').value,
+            author = document.getElementById('author').value,
+            isbn = document.getElementById('isbn').value
+        
+     
+
+        e.preventDefault();
+})
