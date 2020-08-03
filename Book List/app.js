4c8e812485c1624fa959ecfe52cd4c556496dfd5
iamfortune <ikechifortune@gmail.com>
2020-08-03 09:57:01 +0100

    fixed bug that came due to an undefined row

--- Book List/app.js
+++ Book List/app.js
@@ -8,6 +8,14 @@ function Book(title, author, isbn) {
 // UI constructor 
 function UI() {}
 
+UI.prototype.addBookToList = function (book){
+    const list = document.getElementById('book-list');
+
+    // Create Table Element
+    const row = document.createElement('tr');
+   // Inserting columns into the table row
+    row.innerHTML = '';
+}
 
 
 // Event Listeners
@@ -17,8 +25,16 @@ document.getElementById('book-form').addEventListener('submit',
         const title = document.getElementById('title').value,
             author = document.getElementById('author').value,
             isbn = document.getElementById('isbn').value
-        
      
+        // Instantiate book
+        const book = new Book(title, author, isbn);
+        
+        const ui = new UI();
+
+        
+
+        // Add book to List 
+        ui.addBookToList(book);
 
         e.preventDefault();
 })
